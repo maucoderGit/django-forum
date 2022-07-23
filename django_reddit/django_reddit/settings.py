b84557024473774ed025301c4a27b41a333da81e
Mau <dreg5000.bbb@gmail.com>
2022-07-22 22:11:34 -0400

    adding django-discussion-forum.herokuapp.com to allowed urls

--- django_reddit/django_reddit/settings.py
+++ django_reddit/django_reddit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY= os.getenv('SECRET_KEY') ## Write a random secret key to start projec
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://django-discussion-forum.herokuapp.com']
+ALLOWED_HOSTS = ['https://django-discussion-forum.herokuapp.com', "django-discussion-forum.herokuapp.com"]
 
 LOGGING = {
     'version': 1,
