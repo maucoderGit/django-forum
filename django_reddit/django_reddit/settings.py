bc2ad2398c66f4a22d50581a5b9f30bb67b1ce93
Mau <dreg5000.bbb@gmail.com>
2022-07-22 21:34:47 -0400

    typo

--- django_reddit/django_reddit/settings.py
+++ django_reddit/django_reddit/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATOC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 LOGIN_URL = 'users:login'
 
