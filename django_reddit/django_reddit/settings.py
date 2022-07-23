524adf2215e68607e0996a41dc9de9d646f002d2
Mau <dreg5000.bbb@gmail.com>
2022-07-22 21:37:31 -0400

    typo in middlewares fixed

--- django_reddit/django_reddit/settings.py
+++ django_reddit/django_reddit/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.moddleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_reddit.urls'
